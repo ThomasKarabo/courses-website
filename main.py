07b2352ae53732f5e2a59626fdb681f9c0e4c4bb
Karabo Mohlapo <79021656+ThomasKarabo@users.noreply.github.com>
2023-04-06 16:18:47 +0000

    Added boodstrap, json and api

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 main = Flask(__name__)
 
+COURSES = [{
+  'course': 'Physics 3',
+  'Qualification': 'BSc in Physical Sciences',
+  'Fees': 'R 14,000',
+  'Prerequisite': 'Physics 2 and Mathematics 2'
+}, {
+  'course': 'Computer Science 3',
+  'Qualification': 'BSc in Information Technology',
+  'Fees': 'R 9,000',
+  'Prerequisite': 'Computer Science 2 and Mathematics 1'
+}, {
+  'course': 'Mathematics 3',
+  'Qualification': 'BSc in Mathematical Sciences',
+  'Fees': 'R 5,000',
+  'Prerequisite': 'Mathematics 2'
+}, {
+  'course': 'Biology 3',
+  'Qualification': 'BSc in Life Sciences',
+  'Fees': 'R 6,000',
+  'Prerequisite': 'Biology 2'
+}, {
+  'course': 'Chemistry 3',
+  'Qualification': 'BSc in Physical Sciences',
+  'Fees': 'R 11,000',
+  'Prerequisite': 'Chemistry 2 and Mathematics 1'
+}]
+
+
 @main.route('/')
 def hello_world():
-  return render_template("home.html")
+  return render_template("home.html", courses=COURSES)
+
+@main.route('/courses')
+def course_list():
+  return jsonify(COURSES)
 
 if __name__ == "__main__":
-  main.run(host = '0.0.0.0', debug=True)
+  main.run(host='0.0.0.0', debug=True)
