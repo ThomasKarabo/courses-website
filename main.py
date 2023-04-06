cba45474e9b7fd7ed23234a6b6efe8293b55394f
Karabo Mohlapo <79021656+ThomasKarabo@users.noreply.github.com>
2023-04-06 20:19:27 +0000

    Added a logo

--- main.py
+++ main.py
@@ -6,34 +6,39 @@ COURSES = [{
   'course': 'Physics 3',
   'Qualification': 'BSc in Physical Sciences',
   'Fees': 'R 14,000',
-  'Prerequisite': 'Physics 2 and Mathematics 2'
+  'Prerequisite': 'Physics 2 and Mathematics 2',
+  'Hyperlink': 'Physlink'
 }, {
   'course': 'Computer Science 3',
   'Qualification': 'BSc in Information Technology',
   'Fees': 'R 9,000',
-  'Prerequisite': 'Computer Science 2 and Mathematics 1'
+  'Prerequisite': 'Computer Science 2 and Mathematics 1',
+  'Hyperlink': 'CSlink'
 }, {
   'course': 'Mathematics 3',
   'Qualification': 'BSc in Mathematical Sciences',
   'Fees': 'R 6,500',
-  'Prerequisite': 'Mathematics 2'
+  'Prerequisite': 'Mathematics 2',
+  'Hyperlink': 'Mathlink'
 }, {
   'course': 'Biology 3',
   'Qualification': 'BSc in Life Sciences',
   'Fees': 'R 6,000',
-  'Prerequisite': 'Biology 2'
+  'Prerequisite': 'Biology 2',
+  'Hyperlink': 'Biolink'
 }, {
   'course': 'Chemistry 3',
   'Qualification': 'BSc in Physical Sciences',
   'Fees': 'R 11,000',
-  'Prerequisite': 'Chemistry 2 and Mathematics 1'
+  'Prerequisite': 'Chemistry 2 and Mathematics 1',
+  'Hyperlink': 'Chemlink'
 }]
 
 
 @main.route('/')
 def hello_world():
   return render_template("home.html", courses=COURSES)
-
+  
 @main.route('/courses')
 def course_list():
   return jsonify(COURSES)
